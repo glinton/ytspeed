b492b54cad088f7261617eb04c4b577fe46ffa92
greg linton <g2linton@gmail.com>
2020-11-08 17:16:14 -0700

    Commit old work, appears to use message sending

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,17 +1,19 @@
 // listen for keyboard initiated command event (content can't 'hear' them)
-chrome.commands.onCommand.addListener(function(command) {
-	// log all the things
-	console.log('Command:', command);
+chrome.commands.onCommand.addListener(runCmd);
 
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-	    chrome.tabs.sendMessage(tabs[0].id, {cmdName: command}, function(response) {
-			if ( typeof response !== 'undefined' && response ) {
-				console.log("success");
-				console.log(response.done);
-			} else {
-				console.log("error");
-				console.log(chrome.runtime.lastError);
-			}
-		});
-	});
-});
+function runCmd(command) {
+  // log all the things
+  console.log('Command:', command);
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      chrome.tabs.sendMessage(tabs[0].id, {cmdName: command}, function(response) {
+      if ( typeof response !== 'undefined' && response ) {
+        console.log("success");
+        console.log(response.done);
+      } else {
+        console.log("error");
+        console.log(chrome.runtime.lastError);
+      }
+    });
+  });
+}
